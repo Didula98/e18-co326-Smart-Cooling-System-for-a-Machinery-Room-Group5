210a288f4c7c0778a20a76a3d12aae6c6189ac44
Didula Induwara <didulainduwaraamarasinghe@gmail.com>
2023-06-28 20:42:45 +0530

    Modify files

--- database_update/database_update.py
+++ database_update/database_update.py
@@ -13,3 +13,65 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Not conneted")
 
+
+
+
+def on_message(client, userdata, message):
+
+    # print message
+    print("Message:", str(message.payload.decode("utf-8")))
+    print("Topic", str(message.topic))
+
+    if str(message.topic) == "UoP/CO/326/E18/5/SendData":
+        # if requesting data
+        global cursor
+        global connection
+
+        # retrive last 20 rows form the table
+        cursor.execute('''
+        SELECT *
+        FROM (
+        SELECT *, ROW_NUMBER() OVER () AS rn
+        FROM controller_data
+        ) AS subquery
+        WHERE rn > (SELECT COUNT(*) FROM controller_data) - 20;
+        '''
+        )
+
+        data = cursor.fetchall()
+
+        # create json format
+        jsonlist = []
+
+        for row in data:
+            row_dict = {
+                "id": row[0],
+                "temp": row[1],
+                "sent_time": str(row[2]),
+                "received_time": str(row[3]),
+                "rtc_time": str(row[4])
+            }
+
+            jsonlist.append(row_dict)
+
+        # publish data to broker
+        client.publish("UoP/CO/326/E18/5/ReceiveData", str(jsonlist))
+        cursor.close()
+        connection.close()
+
+    
+    elif str(message.topic) == "UoP/CO/326/E18/5/ReceiveData":
+        # if data received from broker
+        data = json.loads(str(message.payload.decode("utf-8")))
+
+        # calculate rtc time
+        rtc_time = datetime.datetime.now().replace(microsecond=0) - datetime.datetime.strptime(data["DateTime"], '%Y-%m-%d %H:%M:%S')
+
+        # insert data to the database
+        sql = "INSERT INTO controller_data VALUES(uuid(), %s, %s, %s, %s)"
+        val = (data["Temperature"], data["DateTime"], datetime.datetime.now().replace(microsecond=0), rtc_time)
+        cursor.execute(sql, val)
+        connection.commit()
+
+
+     
